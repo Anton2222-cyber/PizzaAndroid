312cd63749d6a82e2bc14aff77ed5c387fd1a711
Anton Lashch <lashchanton@gmail.com>
2024-06-22 17:34:03 +0300

    Publish backend

--- AndroidProject/app/src/main/java/com/example/shop/config/Config.java
+++ AndroidProject/app/src/main/java/com/example/shop/config/Config.java
@@ -1,5 +1,6 @@
 package com.example.shop.config;
 
 public class Config {
-    public static final String BASE_URL = "http://192.168.0.20:5174/";
+//    public static final String BASE_URL = "http://192.168.0.20:5174/";
+    public static final String BASE_URL = "https://bomba.novakv.com/";
 }
